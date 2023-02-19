e64836e85cac33279352be65c94a621763db0896
Alberto Bignotti <a.bignotti1@gmail.com>
2023-02-19 16:50:57 +0100

    Add normal and reverse review classes

---
+++ lib/word_mem/review/normal.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module WordMem
+  module Review
+    # Class that models the normal review process of the expression database.
+    # @note A normal review is one where the word in the target language is shown,
+    #   and the user must translate it to the base language
+    class Normal < WordMem::Review::Base
+      private
+
+      # Shows the user a random expression from the expressions in the expression
+      # database that have still not been shown during the review
+      def show_expression
+        @shown_expression = available_elements[rand(available_elements.length)].expression
+        puts @shown_expression
+      end
+
+      # @return [String] The translation of the shown expression
+      def translated_expression
+        @translated_expression = EasyTranslate.translate(
+          @shown_expression, from: target_language, to: base_language
+        )
+      end
+
+      # Updates the expression database for +@shown_expression+, if the self-score
+      # is not 0
+      # @param [Integer] score The self-score to update the shown element with
+      def update_shown_element_with(score)
+        return if score.zero?
+
+        db.increase_review_number_of(@shown_expression, :b2t)
+        db.update_score_of(@shown_expression, :b2t, score)
+        db.persist
+      end
+
+      # Updates the list of elements that can be shown in the next iteration of
+      # the review loop
+      def update_avalable_elements
+        @available_elements -= [db.elements.find { |element| element.expression == @shown_expression }]
+      end
+    end
+  end
+end
---
+++ lib/word_mem/review/reverse.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module WordMem
+  module Review
+    # Class that models the normal review process of the expression database.
+    # @note A reverse review is one where the word in the base language is
+    #   shown, and the user must translate it to the target language
+    class Reverse < WordMem::Review::Base
+      private
+
+      attr_reader :translated_expression
+
+      # Shows the user a random expression from the expressions in the expression
+      # database that have still not been shown during the review
+      def show_expression
+        @translated_expression = available_elements[
+          rand(available_elements.length)
+        ].expression
+
+        @shown_expression = EasyTranslate.translate(
+          @translated_expression, from: target_language, to: base_language
+        )
+
+        puts @shown_expression
+      end
+
+      # Updates the expression database for +@shown_expression+, if the self-score
+      # is not 0
+      # @param [Integer] score The self-score to update the shown element with
+      def update_shown_element_with(score)
+        return if score.zero?
+
+        db.increase_review_number_of(@translated_expression, :t2b)
+        db.update_score_of(@translated_expression, :t2b, score)
+        db.persist
+      end
+
+      # Updates the list of elements that can be shown in the next iteration of
+      # the review loop
+      def update_avalable_elements
+        @available_elements -= [db.elements.find { |element| element.expression == @translated_expression }]
+      end
+    end
+  end
+end
